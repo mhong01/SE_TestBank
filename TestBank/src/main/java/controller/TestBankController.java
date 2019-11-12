48a29602709e72963418580c9a929fac061bbae0
Minh Hong <hellyeyoudo@gmail.com>
2019-11-12 15:20:21 -0800

    Runnable Spring Boot apps

---
+++ TestBank/src/main/java/controller/TestBankController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestBankController {
+	@RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
